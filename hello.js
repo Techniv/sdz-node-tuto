f493ad81d135bd327755afd94cc21d855b75d268
Vincent Peybernes [Techniv] <vpeybernes.pro@gmail.com>
2012-11-21 18:16:43 +0100

    Add part Hello File in hello.js

--- hello.js
+++ hello.js
@@ -7,10 +7,15 @@
  
  console.log('Hello World!');
  
- var http = require('http');
+ var http	= require('http');
+ var fs		= require('fs');
+ 
+ var logFile = fs.createWriteStream('hello.log',{flags: 'a'});
+ 
  var server = http.createServer(function(req, res){
+	logFile.write('Un zéro demande l\'adresse "'+req.url+'" : Je lui dit bonjour !\r\n');
 	console.log('Un zéro demande l\'adresse "'+req.url+'"');
-	res.write("Hello World!");
+	res.write('Hello World!');
 	res.end();
  });
  
