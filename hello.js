09a8aebc14a91144ae9916d0afba2248ac7dd172
Vincent Peybernes [Techniv] <vpeybernes.pro@gmail.com>
2012-11-22 18:22:04 +0100

    orrect indent.

--- hello.js
+++ hello.js
@@ -5,19 +5,18 @@
  * Pour le Site du Zéro <siteduzero.com>
  */
  
- console.log('Hello World!');
- 
- var http	= require('http');
- var fs		= require('fs');
- 
- var logFile = fs.createWriteStream('hello.log',{flags: 'a'});
- 
- var server = http.createServer(function(req, res){
+console.log('Hello World!');
+
+var http	= require('http');
+var fs		= require('fs');
+
+var logFile = fs.createWriteStream('hello.log',{flags: 'a'});
+
+var server = http.createServer(function(req, res){
 	logFile.write('Un zéro demande l\'adresse "'+req.url+'" : Je lui dit bonjour !\r\n');
 	console.log('Un zéro demande l\'adresse "'+req.url+'"');
 	res.write('Hello World!');
 	res.end();
- });
- 
- server.listen(80);
- 
+});
+
+server.listen(80);
